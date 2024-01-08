203e239a0a21e916b9c905f27b8513f87173293e
ZHEN Julie <julie.zhen@telecomnancy.eu>
2024-01-08 18:12:24 +0100

    my profile page + connexion button/pages

--- src/main/java/com/example/codingweek21/LayoutController.java
+++ src/main/java/com/example/codingweek21/LayoutController.java
@@ -1,27 +1,50 @@
 package com.example.codingweek21;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class LayoutController {
 
-    public Menu myProfile;
+    public Label myProfile;
     public Label offers;
     public MenuBar menuBar;
     public Label logout;
 
 
-    public void goToMyProfilePage(MouseEvent actionEvent) {
-        System.out.println("profile");
+    public void goToMyProfilePage(MouseEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("myProfile.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+        Stage modification = (Stage) myProfile.getScene().getWindow();
+        modification.setScene(new Scene(root));
+
     }
 
-    public void goToOffers(MouseEvent actionEvent) {
-        System.out.println("offers");
+    public void goToOffers(MouseEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("allOffers.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+        Stage modification = (Stage) offers.getScene().getWindow();
+        modification.setScene(new Scene(root));
     }
 
-    public void logout(MouseEvent actionEvent) {
-        System.out.println("logout");
+    public void logout(MouseEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("form-login.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+        Stage modification = (Stage) logout.getScene().getWindow();
+        modification.setScene(new Scene(root));
+
     }
 }
--- src/main/java/com/example/codingweek21/Main.java
+++ src/main/java/com/example/codingweek21/Main.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("form-login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("allOffers.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         String css = Objects.requireNonNull(this.getClass().getResource("style.css")).toExternalForm();
         scene.getStylesheets().add(css);
