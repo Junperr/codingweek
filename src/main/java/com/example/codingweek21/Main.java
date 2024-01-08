3711709c7137f70447e0373776e37c2444f2ef74
Anna <anna.galkowski@telecomnancy.eu>
2024-01-08 17:24:05 +0100

    main linked to form-login and css

--- src/main/java/com/example/codingweek21/Main.java
+++ src/main/java/com/example/codingweek21/Main.java
@@ -6,12 +6,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("layout.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("form-login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        String css = Objects.requireNonNull(this.getClass().getResource("style.css")).toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setScene(scene);
         stage.show();
     }
