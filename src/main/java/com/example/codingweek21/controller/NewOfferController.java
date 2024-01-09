71d842e6810c66fb52d86d96ac0dc1dbd7c659a3
DUHEM Aristide <aristide.duhem@telecomnancy.eu>
2024-01-09 11:55:56 +0100

    New Controller for offer creation

---
+++ src/main/java/com/example/codingweek21/controller/NewOfferController.java
@@ -0,0 +1,19 @@
+package com.example.codingweek21.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class NewOfferController {
+
+
+
+    @FXML
+    private TextField firstNameTextField, lastNameTextField, userNameTextField, emailTextField, passWordTextField, addressTextField, cityTextField, zipCodeTextField;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private Button newAccountButton;
+
+}
