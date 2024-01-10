e11026a8212d56e33911e4436ff62cb1247a0027
ugo <ugo.gosso@gmail.com>
2024-01-10 10:59:06 +0100

    push push

---
+++ src/main/java/com/example/codingweek/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.example.codingweek.controller;
+
+import javafx.stage.Stage;
+
+public class OrderController {
+    private Stage stage;
+    private OfferController offerController;
+
+    public void setStage(Stage stage) {this.stage = stage;}
+
+    public void setMainController(OfferController offerController) {this.offerController = offerController;}
+
+
+    public void initData(String address) {
+    }
+}
