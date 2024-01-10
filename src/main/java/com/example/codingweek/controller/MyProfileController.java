ba8b13def635a551fdfc413c4bc6e5822e27dcb0
ugo <ugo.gosso@gmail.com>
2024-01-10 07:26:39 +0100

    minor change profile modification, there are still front-end matters

--- src/main/java/com/example/codingweek/controller/MyProfileController.java
+++ src/main/java/com/example/codingweek/controller/MyProfileController.java
@@ -1,7 +1,7 @@
 package com.example.codingweek.controller;
 
-import com.example.codingweek21.Main;
-import com.example.codingweek21.auth.User;
+import com.example.codingweek.Main;
+import com.example.codingweek.auth.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
