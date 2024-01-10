4be2c496c72878906c179640379e3c3b3f089eeb
ugo <ugo.gosso@gmail.com>
2024-01-10 11:44:46 +0100

    advnce in offer and order

--- src/main/java/com/example/codingweek/controller/OfferController.java
+++ src/main/java/com/example/codingweek/controller/OfferController.java
@@ -1,16 +1,37 @@
 package com.example.codingweek.controller;
 
+import com.example.codingweek.database.DataBase;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class OfferController {
+    @FXML
+    private Button order;
+    @FXML
+    private Label offerPageUser, userPageAvailability, offerPagePrice, offerPageCategory, offerPageDescription, pageOfferTitle;
+    @FXML
+    private ImageView pageOfferImage;
+    private UUID offer_id;
+    private ArrayList<Object> data;
+
+    public void initData(UUID offer_id) {
+        this.offer_id = offer_id;
+
+        DataBase db = DataBase.getInstance();
+        data = db.fetchOne("select * from Offers where id=?", offer_id);
+    }
 
     @FXML
     private void passOrder() throws IOException {
