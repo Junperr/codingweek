285339e42eb958f3f5115aefdeb03f90f71b43a5
ugo <ugo.gosso@gmail.com>
2024-01-11 12:34:27 +0100

    begin OrderDAO

--- src/main/java/com/example/codingweek/DAO/OrderDAO.java
+++ src/main/java/com/example/codingweek/DAO/OrderDAO.java
@@ -1,4 +1,23 @@
 package com.example.codingweek.DAO;
 
+import com.example.codingweek.data.Order;
+import com.example.codingweek.database.DataBase;
+
+import java.util.UUID;
+
 public class OrderDAO {
+    public Order newOrder(Integer cost, String buyer, String seller) {
+        Order order = new Order(cost, buyer, seller);
+        addOrder(order);
+        return order;
+    }
+
+    public void addOrder(Order order) {
+        DataBase db = DataBase.getInstance();
+        db.exec("insert into Orders (id, cost, buyer, seller) values (?,?,?,?)",
+                order.getId(),
+                order.getCost(),
+                order.getBuyer(),
+                order.getSeller());
+    }
 }
