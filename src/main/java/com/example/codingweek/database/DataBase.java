8dbe2cd1c704f9efc2302f98a2a30f0b4c26b718
ZHEN Julie <julie.zhen@telecomnancy.eu>
2024-01-11 01:35:49 +0100

    all offers filter ok, without categories

--- src/main/java/com/example/codingweek/database/DataBase.java
+++ src/main/java/com/example/codingweek/database/DataBase.java
@@ -164,9 +164,9 @@ public class DataBase {
                 exec("INSERT INTO Users (firstName, lastName, userName, email, address , zipCode , city, password, coins) VALUES ('Julie', 'Zhen', 'julieZ', 'julie.zhen@telecomnancy.net', 'address4', '75000', 'city1', '88888888', '5000')");
 
                 // Insert data into Offers
-                exec("INSERT INTO Offers (id, title, type, description, imagePath, price, user, availability) VALUES ('6cc0106a-8d73-4ead-935e-971d00196e6f', 'Pelle à prêter', 'Offer', 'Une belle pelle à prêter', 'pelle.jpg', 10, 'joelD', 1)");
-                exec("INSERT INTO Offers (id, title, type, description, imagePath, price, user, availability) VALUES ('337bc42e-1a1a-4237-a45d-a66d3da4ed03', 'Machine à café à prêter', 'Offer', 'Une belle machine à café à prêter', 'pelle.jpg', 100, 'joelD', 1)");
-                exec("INSERT INTO Offers (id, title, type, description, imagePath, price, user, availability) VALUES ('cfe6e949-e07f-4b87-a0e6-0715db4da09a', 'Machine à thé à prêter', 'Offer', 'Une belle machine à thé à prêter', 'pelle.jpg', 50, 'joelD', 1)");
+                exec("INSERT INTO Offers (id, title, type, description, imagePath, price, user, availability) VALUES ('6cc0106a-8d73-4ead-935e-971d00196e6f', 'Pelle à prêter', 'Loan', 'Une belle pelle à prêter', 'pelle.jpg', 10, 'joelD', 1)");
+                exec("INSERT INTO Offers (id, title, type, description, imagePath, price, user, availability) VALUES ('337bc42e-1a1a-4237-a45d-a66d3da4ed03', 'Machine à café à prêter', 'Loan', 'Une belle machine à café à prêter', 'pelle.jpg', 100, 'joelD', 1)");
+                exec("INSERT INTO Offers (id, title, type, description, imagePath, price, user, availability) VALUES ('cfe6e949-e07f-4b87-a0e6-0715db4da09a', 'Machine à thé à prêter', 'Loan', 'Une belle machine à thé à prêter', 'pelle.jpg', 50, 'joelD', 1)");
 
                 // Insert data into Categories
                 exec("INSERT INTO Categories (offer, category) VALUES ('6cc0106a-8d73-4ead-935e-971d00196e6f', 'Jardinage')");
