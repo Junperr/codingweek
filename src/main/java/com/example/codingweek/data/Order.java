50b01af43fa0d7907d0e90a80bb71ce1c1c7e90e
ugo <ugo.gosso@gmail.com>
2024-01-11 12:34:43 +0100

    begin OrderDAO

--- src/main/java/com/example/codingweek/data/Order.java
+++ src/main/java/com/example/codingweek/data/Order.java
@@ -7,10 +7,34 @@ public class Order {
     private final Integer cost;
     private final String buyer, seller;
 
+    public UUID getId() { return id; }
+
+    public Integer getCost() { return cost; }
+
+    public String getBuyer() { return buyer; }
+
+    public String getSeller() { return seller; }
+
+    public Order(Integer cost, String buyer, String seller) {
+        this.id = UUID.randomUUID();
+        this.cost = cost;
+        this.buyer = buyer;
+        this.seller = seller;
+    }
+
     public Order(UUID id, Integer cost, String buyer, String seller) {
         this.id = id;
         this.cost = cost;
         this.buyer = buyer;
         this.seller = seller;
+    }
+
+    public void printOrder() {
+        System.out.println(
+                "id: " + id + '\n' +
+                "cost: " + cost + '\n' +
+                "buyer: " + buyer + '\n' +
+                "seller: " + seller + '\n'
+        );
     }
 }
