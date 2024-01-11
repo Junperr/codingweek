03aa5a569b8956144500cc7446186d2e7901da51
ugo <ugo.gosso@gmail.com>
2024-01-11 15:28:33 +0100

    Creation of message data and message DAO

---
+++ src/main/java/com/example/codingweek/DAO/MessageDAO.java
@@ -0,0 +1,24 @@
+package com.example.codingweek.DAO;
+
+import com.example.codingweek.data.Message;
+import com.example.codingweek.database.DataBase;
+
+import java.sql.Timestamp;
+
+public class MessageDAO {
+    public Message newMessage(Timestamp timestamp, String content, String sender, String receiver) {
+        Message message = new Message(timestamp, content, sender, receiver);
+        addMessage(Message);
+        return Message;
+    }
+
+    public void addMessage(Message message) {
+        DataBase db = DataBase.getInstance();
+        db.exec("insert into Messages (id, timestamp, content, sender, receiver) values (?,?,?,?,?)",
+                message.getId(),
+                message.getTimestamp(),
+                message.getContent(),
+                message.getSender(),
+                message.getReceiver());
+    }
+}
---
+++ src/main/java/com/example/codingweek/data/Message.java
@@ -0,0 +1,56 @@
+package com.example.codingweek.data;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class Message {
+    private final UUID id;
+    private final Timestamp timestamp;
+    private final String content, sender, receiver;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public Message(Timestamp timestamp, String content, String sender, String receiver) {
+        this.id = UUID.randomUUID();
+        this.timestamp = timestamp;
+        this.content = content;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public Message(UUID id, Timestamp timestamp, String content, String sender, String receiver) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.content = content;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public void printMessage() {
+        System.out.println(
+                "id: " + id + '\n' +
+                        "timestamp: " + timestamp + '\n' +
+                        "content: " + content + '\n' +
+                        "sender: " + sender + '\n' +
+                        "receiver: " + receiver + '\n'
+        );
+    }
+}
