657ee9cdf01476ac21db1443395876946111ecf5
ugo <ugo.gosso@gmail.com>
2024-01-12 07:48:36 +0100

    Fix email regex

--- src/main/java/com/example/codingweek/error/ErrorManager.java
+++ src/main/java/com/example/codingweek/error/ErrorManager.java
@@ -50,7 +50,7 @@ public class ErrorManager {
     }
 
     public void handleInvalidEmail(String email) throws Exception {
-        Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])\n");
+        Pattern emailPattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
 
         if (!emailPattern.matcher(email).find()) {
             errorMessage = "Please enter a valid e-mail address";
@@ -142,7 +142,4 @@ public class ErrorManager {
         }
         raise();
     }
-
-
-//    public void checkMail(Pattern, field)
 }
